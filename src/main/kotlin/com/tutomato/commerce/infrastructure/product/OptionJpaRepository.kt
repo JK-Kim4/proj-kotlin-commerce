35da16b3bb34bf22f7a86b3468a07368d704b065
jongwan <jongbell4@gmail.com>
2025-07-27 01:46:59 +0900

    feat: Product infrastructure

--- src/main/kotlin/com/tutomato/commerce/infrastructure/product/OptionJpaRepository.kt
+++ src/main/kotlin/com/tutomato/commerce/infrastructure/product/OptionJpaRepository.kt
@@ -4,4 +4,9 @@ import com.tutomato.commerce.domain.product.Option
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface OptionJpaRepository : JpaRepository<Option, Long> {
+
+    fun findByProductId(productId: Long): List<Option>
+
+    fun findByProductIdIn(productIds: Set<Long>): List<Option>
+
 }
--- src/main/kotlin/com/tutomato/commerce/infrastructure/product/ProductRepositoryImpl.kt
+++ src/main/kotlin/com/tutomato/commerce/infrastructure/product/ProductRepositoryImpl.kt
@@ -1,10 +1,38 @@
 package com.tutomato.commerce.infrastructure.product
 
+import com.tutomato.commerce.domain.product.Product
+import com.tutomato.commerce.domain.product.ProductRepository
 import org.springframework.stereotype.Repository
+import java.util.*
 
 @Repository
-class ProductRepositoryImpl {
+class ProductRepositoryImpl(
+    private val productJpaRepository: ProductJpaRepository,
+    private val optionJpaRepository: OptionJpaRepository
+) : ProductRepository {
 
-    private lateinit var productJpaRepository: ProductJpaRepository
+    override fun save(product: Product) {
+        productJpaRepository.save(product)
+
+        var options = product.availableOptions
+
+        options!!.setProductId(product.id!!)
+
+        optionJpaRepository.saveAll(options.options)
+    }
+
+    override fun findAll(): List<Product> {
+        var products = productJpaRepository.findAll()
+
+        var productIds = products.map { it.id!! }.toSet()
+
+        var options = optionJpaRepository.findByProductIdIn(productIds)
+
+        return ProductMapper.optionsMappingToProduct(options, products)
+    }
+
+    override fun findById(productId: Long): Optional<Product> {
+        return productJpaRepository.findById(productId);
+    }
 
 }
