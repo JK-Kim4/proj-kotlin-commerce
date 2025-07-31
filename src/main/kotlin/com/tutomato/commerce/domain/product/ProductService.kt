072958fdab393f93f75f5580601124cdf727c12a
jongwan <jongbell4@gmail.com>
2025-07-31 22:20:38 +0900

    refactor: Product 도메인 서비스 수정

--- src/main/kotlin/com/tutomato/commerce/domain/product/ProductService.kt
+++ src/main/kotlin/com/tutomato/commerce/domain/product/ProductService.kt
@@ -1,5 +1,6 @@
 package com.tutomato.commerce.domain.product
 
+import jakarta.persistence.NoResultException
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 import java.time.LocalDateTime
@@ -8,22 +9,29 @@ import java.time.LocalDateTime
 @Transactional
 class ProductService(private val productRepository: ProductRepository) {
 
-    fun save(command : ProductSave) {
-        productRepository.save(command.toEntity())
+    fun save(command: ProductSave): Product {
+        return productRepository.save(command.toEntity())
+    }
+
+    fun save(command: ProductOptionSave): Option {
+        val product = productRepository.findById(command.productId)
+            ?: throw NoResultException("상품이 존재하지않습니다.")
+
+        return productRepository.save(command.toEntity(product))
     }
 
     fun decreaseStock(command : DecreaseStock) {
-        // 옵션 조회
-        productRepository.findById(command.productId)
-            .optionById(command.optionId)
-            .let { option ->
-                option.decreaseStock(command.decreaseAmount)
-                productRepository.save(option)
-            }
+        val option = productRepository.findOptionById(command.optionId)
+            ?: throw NoResultException("상품 옵션이 존재하지않습니다.")
+
+        option.decreaseStock(command.decreaseAmount)
+
+        productRepository.save(option)
     }
 
     fun updateStatus(command : UpdateStatus) {
         val product = productRepository.findById(command.productId)
+            ?: throw NoResultException("상품이 존재하지않습니다.")
 
         when (SaleStatus.valueOf(command.updateStatus)) {
             SaleStatus.SALE_STOPPED -> product.stopSales(LocalDateTime.now())
@@ -35,14 +43,20 @@ class ProductService(private val productRepository: ProductRepository) {
     }
 
     @Transactional(readOnly = true)
-    fun findById(id : Long) : ProductResult.Product =
-        productRepository.findById(id)
-            .let { ProductResult.Product.from(it) }
+    fun findById(id : Long) : ProductResult.Product {
+        val product = productRepository.findById(id)
+            ?: throw NoResultException("상품이 존재하지않습니다.")
+
+        return ProductResult.Product.from(product)
+    }
+
 
     @Transactional(readOnly = true)
-    fun findAll(): ProductResult.Products =
-        productRepository.findAll()
+    fun findAll(): ProductResult.Products {
+        return productRepository.findAll()
             .let { ProductResult.Products.from(it) }
+    }
+
 }
 
 
